7d4ba21a2f91ee8e588b355aa79b0107542c5f2b
Jibanizer <erik1.smekal@st.oth-regensburg.de>
2023-10-05 17:49:22 +0200

    initial creation of directories and files

---
+++ __main__.py
@@ -0,0 +1,4 @@
+# This is the single starting point for the program
+
+if __name__ == "__main__":
+    pass
---
+++ src/sorting.py
@@ -0,0 +1,20 @@
+# Inspiration: https://www.youtube.com/watch?v=ktgxMtWMflU
+
+# Stalin Sort
+
+# Sleep Sort
+
+# Bogo Sort
+
+# Bozo Sort
+
+# Bogobogo Sort
+
+# Quantum Bogo Sort
+
+# Schrödinger Sort
+
+# Intelligent Design Sort
+
+# Miracle Sort
+# Use HolyC?
