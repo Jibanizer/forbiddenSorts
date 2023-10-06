4438de92c9f10ee380ddd55de58b2cae892fc307
Jibanizer <erik1.smekal@st.oth-regensburg.de>
2023-10-07 00:03:03 +0200

    add implementation of stalinSort

--- src/sorting.py
+++ src/sorting.py
@@ -1,6 +1,27 @@
 # Inspiration: https://www.youtube.com/watch?v=ktgxMtWMflU
 
+data = [1, 2, 3, 4, 5]
+data2 = [1, 2, 1, 3, 4, 1, 5]
+data3 = [0, 1, 2, 4, 3, 3, 3, 2, 5, 6, 7, 7, 7, 8, 0, 10]
+
 # Stalin Sort
+def stalinSort(input : list[int]) -> list[int]:
+    """
+    Input: random list of integers
+    Output: sorted list of integers
+    Algorithm: Stalin sends all elements out of order into a Gulag in Siberia. This does possibly reduce the size array. :)
+    """
+    output : list[int] = []
+    temp_val : int = 0
+    for index, value in enumerate(input):
+        if index == 0:
+            output.append(value)
+            temp_val = value
+        else:
+            if value >= temp_val:
+                output.append(value)
+                temp_val = value
+    return output
 
 # Sleep Sort
 
--- src/visualization.py
+++ src/visualization.py
@@ -1,4 +1,5 @@
 import pygame
+from sorting import stalinSort
 
 pygame.init()
 
